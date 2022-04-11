29065eaf15fce89d5181c09c660aa1a7fc21e6c0
olivier villevieille <oliviervillevieille@hotmail.fr>
2022-04-11 10:22:18 +0200

    ajout popup background

--- script.js
+++ script.js
@@ -104,6 +104,54 @@ window.addEventListener("click", function (e) {
     }
   }
 });
+
+// Custom input file
+const wrapper = document.querySelector(".wrapper-photo");
+const fileName = document.querySelector(".file-name");
+const defaultBtn = document.querySelector("#default-btn");
+const customBtn = document.querySelector("#custom-btn");
+const cancelBtn = document.querySelector("#cancel-btn i");
+const img = document.querySelector("img");
+let regExp = /[0-9a-zA-Z\^\&\'\@\{\}\[\]\,\$\=\!\-\#\(\)\.\%\+\~\_ ]+$/;
+function defaultBtnActive() {
+  defaultBtn.click();
+}
+defaultBtn.addEventListener("change", function () {
+  const file = this.files[0];
+  if (file) {
+    const reader = new FileReader();
+    reader.onload = function () {
+      const result = reader.result;
+      img.src = result;
+      wrapper.classList.add("active");
+    };
+    cancelBtn.addEventListener("click", function () {
+      img.src = "";
+      wrapper.classList.remove("active");
+    });
+    reader.readAsDataURL(file);
+  }
+  if (this.value) {
+    let valueStore = this.value.match(regExp);
+    fileName.textContent = valueStore;
+  }
+});
+//Apparition Pop up big background
+let BackgroundBtn = document.querySelector("#background-btn");
+let loginBackground = document.querySelector(".login-background-container");
+let formBackgroundClose = document.querySelector("#form-background-close");
+
+window.onscroll = () => {
+  loginBackground.classList.remove("active");
+};
+BackgroundBtn.addEventListener("click", () => {
+  loginBackground.classList.add("active");
+});
+
+formBackgroundClose.addEventListener("click", () => {
+  loginBackground.classList.remove("active");
+});
+
 //Apparition Pop up police
 let LangBtn = document.querySelector("#polices-btn");
 let loginLang = document.querySelector(".login-police-container");
